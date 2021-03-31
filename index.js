d6cad5817389ff295d91525d906b02c8488194c6
Rohit Handique <rohand.181cv135@nitk.edu.in>
2021-03-31 16:49:36 +0530

    added email service using nodemailer

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const startServer = (new Date()).getTime();
+
+//get stored environment variables
+require('dotenv').config()
+
 const firebase = require("firebase");
 // Required for side-effects
 require("firebase/firestore");
@@ -6,6 +10,10 @@ require("firebase/firestore");
 //importing express and starting it up
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 5000; //defining port number
+
+//importing nodemailer to send emails
+const nodemailer = require('nodemailer');
 
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
@@ -21,7 +29,14 @@ firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 db.settings({ timestampsInSnapshots: true});
 
-const PORT = process.env.PORT || 5000; //defining port numner
+//initialise nodemailer
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+         user: process.env.EMAIL,
+         pass: process.env.PASSWORD
+     }
+ });
 
 //middleware to handle post body
 app.use(express.json()); 
@@ -37,8 +52,7 @@ app.get('/', (req, res) => {
 //takes in a email as the part after forward slash
 //gets the post body
 app.post('/[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$', (req, res) => {
-  
-  let data = Object.assign({}, req.body);
+  /*let data = Object.assign({}, req.body);
   data.timestamp = firebase.firestore.FieldValue.serverTimestamp();
   console.log(data);
   const start = (new Date()).getTime();
@@ -50,7 +64,26 @@ app.post('/[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$', (req, res) => {
     console.error("Error adding document: ", error);
   });
   const end = (new Date()).getTime();
-  console.log(end-start + "ms to run the Firestore database process");
+  console.log(end-start + "ms to run the Firestore database process");*/
+
+
+  //send the req.body data through email
+  //construct the mail (addresses, subject and body)
+  /*const mailOptions = {
+    from: process.env.EMAIL, // sender address
+    to: req.url.slice(1, req.url.length), // list of receivers
+    subject: 'Subject', // Subject line
+    text: JSON.stringify(req.body) // plain text body
+  };
+
+  //function to send the email
+  transporter.sendMail(mailOptions, function (err, info) {
+    if(err)
+      console.log(err)
+    else
+      console.log(info);
+  });*/
+
   res.send(req.body);
 })
 
